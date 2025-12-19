cfc71a2c118fcc77c3fbf27d27b048e70edfd8b1
robwdouglas92 <robwdouglas92@gmail.com>
2025-12-19 09:36:19 +0000

    Create helpers.js

---
+++ lizzie/connections/js/utils/helpers.js
@@ -0,0 +1,32 @@
+export function shuffleArray(array) {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+}
+
+export function formatTime(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}:${secs.toString().padStart(2, '0')}`;
+}
+
+export function getCurrentUserId() {
+    return localStorage.getItem('userId');
+}
+
+export function getCurrentUserName() {
+    return localStorage.getItem('userName');
+}
+
+export function setCurrentUser(userId, userName) {
+    localStorage.setItem('userId', userId);
+    localStorage.setItem('userName', userName);
+}
+
+export function clearCurrentUser() {
+    localStorage.removeItem('userId');
+    localStorage.removeItem('userName');
+}
